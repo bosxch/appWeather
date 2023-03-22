c64c3a5a526c7f5375f483eb42b61623ba61eef9
bosxch <amparocardosobosch@gmail.com>
2023-03-21 22:27:59 -0300

    links added

--- app_weather/src/components/Footer/Footer.jsx
+++ app_weather/src/components/Footer/Footer.jsx
@@ -7,16 +7,16 @@ export const Footer = () => {
     <section className={style.FooterWrapper}>
         <div className={style.SocialIconsContainer}>
             <div className={style.SocialContainer}>
-                <a className={style.SocialIcons}>
+                <a className={style.SocialIcons} href='./downloads/CV_Cardoso_Bosch_Amparo.pdf' target='_blank'>
                     <FcDocument size='3rem'/>
                 </a>
-                <a className={style.SocialIcons}>
+                <a className={style.SocialIcons} href='https://github.com/bosxch' target='_blank'>
                     <AiFillGithub size='3rem'/>
                 </a>
-                <a className={style.SocialIcons}>
+                <a className={style.SocialIcons} href='https://www.linkedin.com/in/amparo-cardoso-bosch/' target='_blank'>
                     <AiFillLinkedin size='3rem'/>
                 </a>
-                <a className={style.SocialIcons}>
+                <a className={style.SocialIcons} href='https://amparo-cardoso-bosch.vercel.app/' target='_blank'>
                     <BsBriefcase size='3rem'/>
                 </a>
             </div>
