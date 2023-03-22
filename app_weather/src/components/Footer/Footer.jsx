f556b674dda6d3a76d57ab91d6ff249e12589a15
bosxch <amparocardosobosch@gmail.com>
2023-03-21 22:59:42 -0300

    changes in Footer and version beta of Header component

--- app_weather/src/components/Footer/Footer.jsx
+++ app_weather/src/components/Footer/Footer.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import { FcDocument } from "react-icons/fc";
 import {BsBriefcase} from 'react-icons/bs'
 
 export const Footer = () => {
     <section className={style.FooterWrapper}>
         <div className={style.SocialIconsContainer}>
             <div className={style.SocialContainer}>
-                <a className={style.SocialIcons} href='./downloads/CV_Cardoso_Bosch_Amparo.pdf' target='_blank'>
-                    <FcDocument size='3rem'/>
-                </a>
                 <a className={style.SocialIcons} href='https://github.com/bosxch' target='_blank'>
                     <AiFillGithub size='3rem'/>
                 </a>
---
+++ app_weather/src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import { style } from '@mui/system';
+import Link from 'next/link';
+import React from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import {BsBriefcase} from 'react-icons/bs'
+
+import style from './Header.module.css'
+
+export const Header = () =>  (
+ <div className={style.Container}> 
+    <div className={style.DivApp}>
+      <Link href='/'>
+        <a style={{display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px'}}>
+        <span className={style.Span} style={{'margin-top':'0.5rem', 'cursor':'default'}}>App Weather</span>
+        </a>
+      </Link>
+    </div>
+    <div className={style.DivRefs}>
+      <li>
+        <Link href='#locations'>
+          <a className={style.NavLink}>My Locations</a>
+        </Link>
+      </li>
+      <li>
+        <Link href='#tech'>
+          <a className={style.NavLink}>Add Location</a>
+        </Link>
+      </li>
+    </div>
+    <div className={style.DivSocialMedia}>
+        <a className={style.SocialIcons} href='https://github.com/bosxch' target='_blank'>
+            <AiFillGithub size='3rem'/>
+        </a>
+        <a className={style.SocialIcons} href='https://www.linkedin.com/in/amparo-cardoso-bosch/' target='_blank'>
+            <AiFillLinkedin size='3rem'/>
+        </a>
+        <a className={style.SocialIcons} href='https://amparo-cardoso-bosch.vercel.app/' target='_blank'>
+            <BsBriefcase size='3rem'/>
+        </a>
+    </div>
+ </div>
+);
+
+export default Header;
